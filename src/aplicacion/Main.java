33caa6def374ce90a18c44410e7ad52c6f847650
AscCrs <cristoe2003@gmail.com>
2023-06-09 12:39:34 -0600

    Se añadio el archivo principal para controlar las vistas

---
+++ src/aplicacion/Main.java
@@ -0,0 +1,14 @@
+package aplicacion;
+
+import vistas.VistaPrincipal;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+        VistaPrincipal principal = new VistaPrincipal();
+        principal.setVisible(true);
+    }
+    
+}
